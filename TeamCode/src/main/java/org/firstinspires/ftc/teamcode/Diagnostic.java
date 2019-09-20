291c6403fcfe01a2280580cd053c3f022b9c22fb
ftcdontblink <46360500+ftcdontblink@users.noreply.github.com>
2019-09-19 21:07:21 -0400

    Diagnostic code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostic.java
@@ -0,0 +1,118 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.Scanner;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Diagnostic", group="Linear Opmode")
+@Disabled
+public class Diagnostic extends LinearOpMode {
+
+    int diagnostic = 1; // setting diagnostic state for the switch system
+    public DcMotor lFront;
+    public DcMotor lBack;
+    public DcMotor rFront;
+    public DcMotor rBack;
+
+    @Override
+    public void runOpMode() {
+
+         lFront = hardwareMap.get(DcMotor.class, "lFront"); // defining motors
+         rFront = hardwareMap.get(DcMotor.class, "rFront");
+         lBack = hardwareMap.get(DcMotor.class, "lBack");
+         rBack = hardwareMap.get(DcMotor.class, "rBack");
+
+
+         lFront.setDirection(DcMotor.Direction.REVERSE);
+         lBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+        telemetry.addData("Status", "Initialized"); // showing that the robot has been initialized
+        telemetry.addData("Diagnostic Program", "No boot problems"); // furthermore showing the user the initialization and showing no boot problems
+        telemetry.update();
+
+        waitForStart();
+
+
+        switch(diagnostic){
+
+            case 1: // first case for the switch machine, will make it so that we can run all motors at the same time (basic driving)
+
+
+                lBack.setPower(-gamepad1.left_stick_y); // setting mode to basic driving for all motors
+                lBack.setPower(-gamepad1.left_stick_y);
+                rBack.setPower(-gamepad1.right_stick_y);
+                rFront.setPower(-gamepad1.right_stick_y);
+
+                if(gamepad1.a){
+                    diagnostic = 2;
+                }
+                if(gamepad1.b){
+                    diagnostic = 3;
+                }
+                if(gamepad1.x){
+                    diagnostic = 4;
+                }
+                if(gamepad1.y){
+                    diagnostic = 5;
+                }
+
+
+
+        }
+
+
+
+
+
+        }
+    }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test_opMode.java
+++
@@ -1,1 +0,0 @@
-//yeeeeeeeet yeeeeeeeeeeeet
