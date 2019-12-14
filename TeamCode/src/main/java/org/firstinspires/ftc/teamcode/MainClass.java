5fa719ded698c535d209136e4f600319408f081c
ftcdontblink <ftcdontblink@gmail.com>
2019-12-14 14:33:26 -0500

    Resolving merge conflicts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainClass.java
@@ -1,60 +1,33 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+        import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.DcMotorSimple;
+        import com.qualcomm.robotcore.hardware.HardwareMap;
+        import com.qualcomm.robotcore.hardware.Servo;
+        import com.qualcomm.robotcore.util.ElapsedTime;
+        import com.qualcomm.robotcore.util.Range;
 
 
 /**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in
+ * either the autonomous or the teleop period of an FTC match. The names of OpModes appear on the
+ * menu of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
  *
  * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
  * It includes all the skeletal structure that all linear OpModes contain.
  *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new
+ * name. Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode
+ *list
  */
 
 public class MainClass extends LinearOpMode {
 
+    // Defining Motors
     public DcMotor lFrontMotor;
     public DcMotor rFrontMotor;
     public DcMotor rBackMotor;
@@ -62,7 +35,20 @@ public class MainClass extends LinearOpMode {
     public Servo ServoLeft;
     public Servo ServoRight;
     public Servo ServoStone;
+    public DcMotor LeftIntake;
+    public DcMotor RightIntake;
+    public DcMotor Pivot;
+    public Servo Flip1;
+    public Servo Flip2;
+    public DcMotor Lift;
+    static final double     COUNTS_PER_MOTOR_REV_LET    = 28;
+    static final double     DRIVE_GEAR_REDUCTION_LET    = 26.9;
+    static final double     FINAL_DRIVE_REDUCTION_LET   = 2.0;
+    static final double     WHEEL_DIAMETER_INCHES_LET   = 4.0;
+    static final double     LET = (COUNTS_PER_MOTOR_REV_LET * FINAL_DRIVE_REDUCTION_LET *
+            DRIVE_GEAR_REDUCTION_LET)/ (Math.PI * WHEEL_DIAMETER_INCHES_LET);
 
+    //Defining Variables
     public double lFrontSpeed;
     public double lBackSpeed;
     public double rFrontSpeed;
@@ -79,8 +65,20 @@ public class MainClass extends LinearOpMode {
     public double rightStartAngle = 0.75;
     public double leftterminalAngle = 0.6;
     public double rightterminalAngle = 0.15;
-    public double stoneStartAngle = 0.5;
-    public double stoneterminalAngle = 0.95;
+    public double stoneStartAngle = 0.35;
+    public double stoneterminalAngle = 0.9;
+    public int PivotArmAngle; //TODO: Add/change values for this to be accurate (Line 231
+    public int PivotArmTerminalAngle; //TODO: Add.change values - may need to cast in Mecanum_Drive
+
+    public int flip = 1;
+
+    public int firstflipstart;
+    public int firstflipterm;
+    public int secondflipstart;
+    public int secondflipterm;
+
+    public int position = 0;
+    public int add = 0;         //subjective to change
 
     //Setting Motor values
     public ElapsedTime     runtime = new ElapsedTime();
@@ -89,11 +87,12 @@ public class MainClass extends LinearOpMode {
     static final double     DRIVE_GEAR_REDUCTION    = 26.9;
     static final double     FINAL_DRIVE_REDUCTION   = 2.0;
     static final double     WHEEL_DIAMETER_INCHES   = 4.0;
-    static final double     COUNTS_PER_INCH =(COUNTS_PER_MOTOR_REV * FINAL_DRIVE_REDUCTION* DRIVE_GEAR_REDUCTION)/ (Math.PI*WHEEL_DIAMETER_INCHES);
+    static final double     COUNTS_PER_INCH =(COUNTS_PER_MOTOR_REV * FINAL_DRIVE_REDUCTION*
+            DRIVE_GEAR_REDUCTION)/ (Math.PI*WHEEL_DIAMETER_INCHES);
     static final double     DRIVE_SPEED             = 0.3;
     static final double     TURN_SPEED              = 0.2;
 
-    HardwareMap hwMap;
+    HardwareMap asn;
 
     public MainClass() {
 
@@ -104,8 +103,8 @@ public class MainClass extends LinearOpMode {
 
     }
 
-    public void init (HardwareMap h) {
-        hwMap = h;
+    public void init(HardwareMap h) {
+        HardwareMap hwMap = h;
         lFrontMotor = hwMap.get(DcMotor.class, "left_Front_Motor");
         rFrontMotor = hwMap.get(DcMotor.class, "right_Front_Motor");
         lBackMotor = hwMap.get(DcMotor.class, "left_Back_Motor");
@@ -113,17 +112,20 @@ public class MainClass extends LinearOpMode {
         ServoLeft = hwMap.get(Servo.class, "servo_left");
         ServoRight = hwMap.get(Servo.class, "servo_right");
         ServoStone = hwMap.get(Servo.class, "servo_stone");
+        Flip1 = hwMap.get(Servo.class, "flip_1");
+//        Flip2 = hwMap.get(Servo.class, "flip_2"); //TODO: Uncomment
+        LeftIntake = hwMap.get(DcMotor.class, "left_intake");
+        RightIntake = hwMap.get(DcMotor.class, "right_intake");
 
 
         //Set left motors to reverse
-        rBackMotor.setDirection(DcMotor.Direction.REVERSE);
         rFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rBackMotor.setDirection(DcMotor.Direction.REVERSE);
 
         rFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rBackMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lBackMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
 
         lFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -140,34 +142,89 @@ public class MainClass extends LinearOpMode {
         rBackMotor.setPower(0);
         lFrontMotor.setPower(0);
         lBackMotor.setPower(0);
+
+        ServoLeft.setPosition(leftstartAngle);
+        ServoRight.setPosition(rightStartAngle);
+
+        ServoStone.setPosition(stoneStartAngle);
+
+        Flip1.setPosition(0.2);
     }
 
-    public void buildingZoneRed() {
-        EncoderMove(-5);
-        EncoderStrafe(-36);
-
+    public void buildingZoneRed(boolean op) {
+        EncoderMove(-5, op);
+        EncoderStrafe(-8, op);
+        EncoderMove(-24, op);
+        sleep(1000);
+        ServoLeft.setPosition(leftterminalAngle);
+        ServoRight.setPosition(rightterminalAngle);
+        sleep(1000);
+        EncoderMove(32, op);
+        sleep(500);
+        ServoLeft.setPosition(leftstartAngle);
+        ServoRight.setPosition(rightStartAngle);
+        sleep(500);
+        EncoderStrafe(20, op);
+        EncoderMove(-16, op);
+        EncoderStrafe(-10, op);
+        EncoderStrafe(25, op);
     }
 
-    public void buildingZoneBlue() {
-        EncoderMove(-5);
-        EncoderStrafe(36);
+
+    public void buildingZoneBlue(boolean op) {
+        EncoderMove(-5, op);
+        EncoderStrafe(8, op);
+        EncoderMove(-24, op);
+        sleep(1000);
+        ServoLeft.setPosition(leftterminalAngle);
+        ServoRight.setPosition(rightterminalAngle);
+        sleep(1000);
+        EncoderMove(32, op);
+        sleep(500);
+        ServoLeft.setPosition(leftstartAngle);
+        ServoRight.setPosition(rightStartAngle);
+        sleep(500);
+        EncoderStrafe(-20, op);
+        EncoderMove(-16, op);
+        EncoderStrafe(10, op);
+        EncoderStrafe(-25, op);
     }
 
-    public void loadingZoneRed() {
-        EncoderStrafe( -26);
-        EncoderMove(36);
+    public void loadingZoneRed(boolean op) {
+        EncoderStrafe( -26, opModeIsActive());
+        EncoderMove(36, opModeIsActive());
     }
 
-    public void loadingZoneBlue() {
-        EncoderStrafe( -26);
-        EncoderMove(-36);
+    public void loadingZoneBlue(boolean op) {
+        EncoderStrafe(-33, opModeIsActive());
+        sleep(1000);
+        ServoStone.setPosition(0.95);
+        sleep(1000);
+        EncoderStrafe(30, opModeIsActive());
+        EncoderMove(-38, opModeIsActive());
+        sleep(1000);
+        ServoStone.setPosition(0.5);
+        sleep(1000);
+        EncoderMove(46, opModeIsActive());
+        EncoderStrafe(-32, opModeIsActive());
+        sleep(1000);
+        ServoStone.setPosition(0.95);
+        sleep(1000);
+        EncoderStrafe(28, opModeIsActive());
+        sleep(1000);
+        ServoStone.setPosition(0.5);
+        sleep(1000);
+        EncoderMove(16, opModeIsActive());
+
     }
 
     public void SafetyZoneRed() {}
 
     public void SafetyZoneBlue() {}
 
-    public void EncoderMove(int inches) {
+    public void EncoderMove(int inches, boolean opMode) {
+        boolean op = opMode;
+
         int newLeftFrontTarget, newLeftBackTarget;
         int newRightFrontTarget, newRightBackTarget;
 
@@ -192,23 +249,25 @@ public class MainClass extends LinearOpMode {
 
         //TODO Wouldnt this actually run the motors and be the motion in the program?
         runtime.reset();
-        lFrontMotor.setPower(Math.abs(0.5));
-        lBackMotor.setPower(Math.abs(0.5));
-        rBackMotor.setPower(Math.abs(0.5));
-        rFrontMotor.setPower(Math.abs(0.5));
+        lFrontMotor.setPower(Math.abs(0.6));
+        lBackMotor.setPower(Math.abs(0.6));
+        rBackMotor.setPower(Math.abs(0.6));
+        rFrontMotor.setPower(Math.abs(0.6));
 
-        while (opModeIsActive() &&
+        while (op == true &&
                 (runtime.seconds() < 30) &&
-                (lFrontMotor.isBusy() && lBackMotor.isBusy() || rFrontMotor.isBusy() && rBackMotor.isBusy())) {
+                (lFrontMotor.isBusy() && lBackMotor.isBusy() || rFrontMotor.isBusy() &&
+                        rBackMotor.isBusy())) {
             //TODO The isBusy check is at the beggining of the while opModeIsActive
             // Display it for the driver.
-            telemetry.addData("Path1",  "Running to %7d :%7d", newLeftFrontTarget,  newRightFrontTarget);
-            telemetry.addData("Path2",  "Running at %7d :%7d:%7d :%7d",
-                    lFrontMotor.getCurrentPosition(),
-                    lBackMotor.getCurrentPosition(),
-                    rBackMotor.getCurrentPosition(),
-                    rFrontMotor.getCurrentPosition());
-            telemetry.update();
+//            telemetry.addData("Path1",  "Running to %7d :%7d", newLeftFrontTarget,
+//            newRightFrontTarget);
+//            telemetry.addData("Path2",  "Running at %7d :%7d:%7d :%7d",
+//                    lFrontMotor.getCurrentPosition(),
+//                    lBackMotor.getCurrentPosition(),
+//                    rBackMotor.getCurrentPosition(),
+//                    rFrontMotor.getCurrentPosition());
+//            telemetry.update();
         }
 
         // Stop all motion;
@@ -218,7 +277,8 @@ public class MainClass extends LinearOpMode {
         rBackMotor.setPower(0);
     }
 
-    public void EncoderStrafe(int inches) {
+    public void EncoderStrafe(int inches, boolean op) {
+
         int newLeftFrontTarget, newLeftBackTarget;
         int newRightFrontTarget, newRightBackTarget;
 
@@ -248,18 +308,20 @@ public class MainClass extends LinearOpMode {
         rBackMotor.setPower(Math.abs(0.5));
         rFrontMotor.setPower(Math.abs(0.5));
 
-        while (opModeIsActive() &&
+        while (op == true &&
                 (runtime.seconds() < 30) &&
-                (lFrontMotor.isBusy() && lBackMotor.isBusy() || rFrontMotor.isBusy() && rBackMotor.isBusy())) {
+                (lFrontMotor.isBusy() && lBackMotor.isBusy() && rFrontMotor.isBusy() &&
+                        rBackMotor.isBusy())) {
             //TODO The isBusy check is at the beggining of the while opModeIsActive
             // Display it for the driver.
-            telemetry.addData("Path1",  "Running to %7d :%7d", newLeftFrontTarget,  newRightFrontTarget);
-            telemetry.addData("Path2",  "Running at %7d :%7d:%7d :%7d",
-                    lFrontMotor.getCurrentPosition(),
-                    lBackMotor.getCurrentPosition(),
-                    rBackMotor.getCurrentPosition(),
-                    rFrontMotor.getCurrentPosition());
-            telemetry.update();
+//            telemetry.addData("Path1",  "Running to %7d :%7d", newLeftFrontTarget,
+//                                                              newRightFrontTarget);
+//            telemetry.addData("Path2",  "Running at %7d :%7d:%7d :%7d",
+//                    lFrontMotor.getCurrentPosition(),
+//                    lBackMotor.getCurrentPosition(),
+//                    rBackMotor.getCurrentPosition(),
+//                    rFrontMotor.getCurrentPosition());
+//            telemetry.update();
         }
 
         // Stop all motion;
